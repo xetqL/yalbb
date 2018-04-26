2e29f8c1d40b8e531ce62b5eab3cc011f9548e9e
Anthony Boulmier <anthony.boulmier@hesge.ch>
2018-04-26 14:09:51 +0200

    bash file

--- baobab.sh
+++ baobab.sh
@@ -3,24 +3,10 @@ PE=$1
 PARTICLES=$2
 NBIT=$3
 CYCLE=$(( NBIT / 100 ))
-NAME=$4
-if [ -z "$5" ]; then
-    CURRENTCALL=0
-else
-    CURRENTCALL=$4
-fi
-
-if [ -z "$6" ]; then
-    DEST="BAOBAB"
-else
-    DEST="LOCAL"
-fi
+CURRENTCALL=$4
+PART=$5
 
 while [ $CURRENTCALL -lt $NBIT ]; do
-    if [ "$DEST" == "LOCAL" ]; then
-        mpirun -np $1 bin/dataset -p $1 -n $2 -g 2.0 -f $3 -F 1 -d 2.0 -S 19937 -C $CURRENTCALL
-    else
-        sbatch -n $PE -p parallel -t 2-00:00:00 -J $NAME ljmpi.slurm $PE $PARTICLES $NBIT $CURRENTCALL
-    fi
+    sbatch -n $PE -p $5 -t 0-00:05:00 ljmpi.slurm $PE $PARTICLES $NBIT $CURRENTCALL
     let CURRENTCALL=$CURRENTCALL+100
 done
