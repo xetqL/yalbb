8db5b5f9ca7d9e6271614ccfe7e97a8fb5fbc322
Anthony Boulmier <anthony.boulmier@unige.ch>
2020-05-21 13:38:52 +0200

    working

--- includes/yalbb/cll.hpp
+++ includes/yalbb/cll.hpp
@@ -23,11 +23,9 @@ inline void CLL_update(Integer acc,
         auto n_els  = span.second;
         for (size_t i = 0; i < n_els; ++i) {
             auto& pos = *getPositionFunc(&el_ptr[i]);
-            if(is_within<N>(bbox, pos)) {
-                c = CoordinateTranslater::translate_position_into_local_index<N>(pos, rc, bbox, lc[0], lc[1]);
-                lscl->at(i + acc) = head->at(c);
-                head->at(c) = i + acc;
-            }
+            c = CoordinateTranslater::translate_position_into_local_index<N>(pos, rc, bbox, lc[0], lc[1]);
+            lscl->at(i + acc) = head->at(c);
+            head->at(c) = i + acc;
         }
         acc += n_els;
     }
