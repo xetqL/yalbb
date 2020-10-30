183e0543e7c90d39037852c7a1d4898f51cc3e36
Anthony Boulmier <anthony.boulmier@unige.ch>
2020-10-30 14:58:21 +0100

    add prefix param

--- includes/yalbb/params.hpp
+++ includes/yalbb/params.hpp
@@ -20,26 +20,28 @@
  *@c*/
 struct sim_param_t {
 
-    std::string fname;   /* File name (run.out)        */
-    int   npart;   /* Number of particles (500)  */
-    int   nframes; /* Number of frames (200)     */
-    int   npframe; /* Steps per frame (100)      */
-    float dt;      /* Time step (1e-4)           */
-    float eps_lj;  /* Strength for L-J (1)       */
-    float sig_lj;  /* Radius for L-J   (1e-2)    */
-    float G;       /* Gravitational strength (1) */
-    float T0;      /* Initial temperature (1)    */
-    float simsize; /* Borders of the simulation  */
-    float rc;      /* factor multiplying sigma for cutoff */
-    float bounce;  /* shock absorption factor (0=no bounce, 1=full bounce) */
-    bool  record;  /* record the simulation in a binary file */
+    int   npart;        /* Number of particles (500)  */
+    int   nframes;      /* Number of frames (200)     */
+    int   npframe;      /* Steps per frame (100)      */
+    float dt;           /* Time step (1e-4)           */
+    float eps_lj;       /* Strength for L-J (1)       */
+    float sig_lj;       /* Radius for L-J   (1e-2)    */
+    float G;            /* Gravitational strength (1) */
+    float T0;           /* Initial temperature (1)    */
+    float simsize;      /* Borders of the simulation  */
+    float rc;           /* factor multiplying sigma for cutoff */
+    float bounce;       /* shock absorption factor (0=no bounce, 1=full bounce) */
+    bool  record;       /* record the simulation in a binary file */
     bool  import = false;
-    int   seed;    /* seed used in the RNG */
+    int   seed;         /* seed used in the RNG */
     int   particle_init_conf = 1;
     int   id = 0;
     int   nb_best_path = 1;
 
     std::string uuid;
+    std::string prefix;
+    std::string fname;  /* File name (run.out)        */
+
     int verbosity;
 };
 
--- src/params.cpp
+++ src/params.cpp
@@ -35,19 +35,22 @@ std::optional<sim_param_t> get_params(int argc, char** argv){
     parser.add_opt_value('B', "best", params.nb_best_path, 1, "Number of Best path to retrieve (A*)", "INT").require();
     parser.add_opt_value('b', "bounce", params.bounce, 1.0f, "Bouncing factor (1=full bounce, 0=no bounce)", "FLOAT");
     parser.add_opt_value('d', "distribution", params.particle_init_conf, 1, "Initial particle distribution 1: Uniform, 2:Half, 3:Wall, 4: Cluster", "INT");
+
     parser.add_opt_value('e', "epslj", params.eps_lj, 1.0f, "Epsilon (lennard-jones)", "FLOAT");
     parser.add_opt_value('f', "npframe", params.npframe, 100, "steps per frame", "INT").require();
     parser.add_opt_value('F', "nframes", params.nframes, 100, "number of frames", "INT").require();
-    parser.add_opt_value('g', "gravitation", params.G, 1.0f, "Gravitational strength", "FLOAT");
+    parser.add_opt_value('g', "gravitation", params.G, 1.0f, "Number of G's", "FLOAT");
     parser.add_opt_value('i', "id", params.id, 0, "Simulation id", "INT").require();
     parser.add_opt_value('I', "import", params.fname, std::string("particles.in"), "import particles from this file", "STRING");
     parser.add_opt_value('l', "lattice", params.rc, 2.5f, "Lattice size", "FLOAT");
     parser.add_opt_value('n', "nparticles", params.npart, 500, "Number of particles", "INT").require();
-     parser.add_opt_flag('r', "record", "Record the simulation", &params.record);
+    parser.add_opt_value('p', "prefix", params.prefix, "", "prefix for results", "STRING");
+
+    parser.add_opt_flag('r', "record", "Record the simulation", &params.record);
     parser.add_opt_value('s', "siglj", params.sig_lj, 1e-2f, "Sigma (lennard-jones)", "FLOAT");
     parser.add_opt_value('S', "seed", params.seed, rand(), "Random seed", "INT").require();
-    parser.add_opt_value('t', "dt", params.dt, 1e-5f, "Time step", "float");
-    parser.add_opt_value('T', "temperature", params.T0, 1.0f, "Initial temperatore", "float");
+    parser.add_opt_value('t', "dt", params.dt, 1e-5f, "Time step", "FLOAT");
+    parser.add_opt_value('T', "temperature", params.T0, 1.0f, "Initial temperatore", "FLOAT");
     parser.add_opt_value('w', "width", params.simsize, 1.0f, "Simulation box width", "FLOAT");
 
     bool output;
@@ -60,10 +63,10 @@ std::optional<sim_param_t> get_params(int argc, char** argv){
         return std::nullopt;
     }
 
-    params.import = (bool) parser.count('I');
+    params.import    = (bool) parser.count('I');
     params.verbosity = verbose.get_count();
     params.rc *= params.sig_lj;
-    params.G *= 9.81f;
+    params.G  *= 9.81f;
 
     return params;
 }
